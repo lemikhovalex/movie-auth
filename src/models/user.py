d430c0903e5814d7f5eece51d5beefedc6b52bcc
Aleksandr Lemikhov <lemikhovalex@yandex.ru>
2022-04-21 17:36:12 +0300

    user model for new db

--- src/models/user.py
+++ src/models/user.py
@@ -1,27 +1,22 @@
 import uuid
 
-from sqlalchemy import Column, String
 from sqlalchemy.dialects.postgresql import UUID
 
-from db.pg import PostgresDB
+from db.pg import db
 
 
-class User(PostgresDB):
+class User(db.Model):
     __tablename__ = "users"
 
-    id = Column(
+    id = db.Column(
         UUID(as_uuid=True),
         primary_key=True,
         default=uuid.uuid4,
         unique=True,
         nullable=False,
     )
-    login = Column(String, unique=True, nullable=False)
-    password = Column(String, nullable=False)
-
-    def __init__(self, login, password):
-        self.login = login
-        self.password = password
+    login = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
 
     def __repr__(self):
         return f"<User {self.login}>"
