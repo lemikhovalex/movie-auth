2575f8b5a4ede9c1daf4c52435f2f917d018f167
Aleksandr Lemikhov <lemikhovalex@yandex.ru>
2022-04-21 20:32:56 +0300

    finer admin creation and mentioned it in readme

--- src/main_build_tables.py
+++ src/main_build_tables.py
@@ -1,10 +1,8 @@
-from sqlalchemy.exc import IntegrityError
-
 from app import app
 from db.pg import db
 from models.roles import UsersRoles
 from models.sessions import Session
-from models.user import User
+from models.user import User, UserData
 
 # create sql alchemy
 app.app_context().push()
@@ -12,12 +10,9 @@ db.create_all()
 db.session.commit()
 
 # create admin
-try:
-    admin = User(login="admin", password="password")
-    db.session.add(admin)
-    db.session.commit()
-except IntegrityError:
-    pass
+admin = User(login="admin", password="password")
+db.session.add(admin)
+
 
 # give him admin rights
 
@@ -25,16 +20,20 @@ User.query.all()
 admin = User.query.filter_by(login="admin").first()
 admin_u_id = admin.id
 
-try:
-    role = UsersRoles(
-        user_id=admin_u_id,
-        role_id=0,
-    )
-    db.session.add(role)
-    db.session.commit()
-except IntegrityError:
-    pass
+# set some admin data
+u_data = UserData(
+    user_id=admin_u_id, first_name="A", second_name="Dmin", email="a@dm.in"
+)
+db.session.add(u_data)
 
+# give him role
+role = UsersRoles(
+    user_id=admin_u_id,
+    role_id=0,
+)
+db.session.add(role)
+
+# and add session
 ses = Session(user_id=admin_u_id, agent="device")
 db.session.add(ses)
 db.session.commit()
