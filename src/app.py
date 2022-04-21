855c55a5ccfca9dccec3d734072a834ae25df4cd
Aleksandr Lemikhov <lemikhovalex@yandex.ru>
2022-04-21 17:35:40 +0300

    sql alchemy init and creation + admin

--- src/app.py
+++ src/app.py
@@ -1,9 +1,26 @@
 from flask import Flask
 from flask_marshmallow import Marshmallow
+from sqlalchemy.exc import IntegrityError
 
 from api import api_bp
+from db.pg import db, init_db
+from models.user import User
 
 app = Flask(__name__)
 app.register_blueprint(api_bp)
+
+# create sql alchemy
+init_db(app)
+app.app_context().push()
+db.create_all()
+db.session.commit()
+
+# create admin
+try:
+    admin = User(login="admin", password="password")
+    db.session.add(admin)
+    db.session.commit()
+except IntegrityError:
+    pass
 
 ma = Marshmallow(app)
--- src/db/pg.py
+++ src/db/pg.py
@@ -1,3 +1,18 @@
-from sqlalchemy.ext.declarative import declarative_base
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-PostgresDB = declarative_base()
+from config.db import PG_DB_NAME, PG_PASSWORD, PG_USER_NAME
+
+db = SQLAlchemy()
+
+
+def init_db(app: Flask):
+    app.config[
+        "SQLALCHEMY_DATABASE_URI"
+    ] = "postgresql://{user}:{pasword}@{host}/{db_name}".format(
+        user=PG_USER_NAME,
+        pasword=PG_PASSWORD,
+        db_name=PG_DB_NAME,
+        host="db",
+    )
+    db.init_app(app)
