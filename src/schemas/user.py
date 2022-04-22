94bb2ba9600c4ae3334cde7d9cbf67e01fb71415
Aleksandr Lemikhov <lemikhovalex@yandex.ru>
2022-04-22 17:16:02 +0300

    schema for user regestration data validation

--- src/schemas/user.py
+++ src/schemas/user.py
@@ -1,10 +1,41 @@
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow_sqlalchemy.fields import Nested
+
 from app import ma
+from models.user import UserCredentials, UserData
 
 
-class UserSchema(ma.Schema):
+class UserCredentialsSch(SQLAlchemyAutoSchema):
     class Meta:
-        fields = ("login", "password")
+        model = UserCredentials
+        include_relationships = True
+
+    id = ma.auto_field()
+    login = ma.auto_field()
+    password = ma.auto_field()
 
 
-user_schema = UserSchema()
-users_schema = UserSchema(many=True)
+class UserDataSch(SQLAlchemyAutoSchema):
+    class Meta:
+        model = UserData
+        include_relationships = True
+
+    user_id = ma.auto_field()
+    first_name = ma.auto_field()
+    second_name = ma.auto_field()
+
+
+class RegisterSchema(SQLAlchemyAutoSchema):
+    credentials = Nested(
+        UserCredentialsSch(
+            exclude=("id",),
+        ),
+    )
+    user_data = Nested(
+        UserDataSch(
+            exclude=("user_id",),
+        ),
+    )
+
+
+register_schema = RegisterSchema()
