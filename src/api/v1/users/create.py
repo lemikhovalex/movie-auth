759942cb2838a3ee07818b7bc722a1d6f5f5b974
Aleksandr Lemikhov <lemikhovalex@yandex.ru>
2022-04-22 17:17:03 +0300

    validate request data

--- src/api/v1/users/create.py
+++ src/api/v1/users/create.py
@@ -1,27 +1,40 @@
 import uuid
 
-from flask import Blueprint, request
+from flask import Blueprint, jsonify, request
+from marshmallow import ValidationError
+from sqlalchemy.exc import IntegrityError
 
 from db.pg import db
 from models.user import UserCredentials, UserData
+from schemas.user import register_schema
 
 create_bp = Blueprint("create", __name__, url_prefix="")
 
 
 @create_bp.route("", methods=["POST"])
 def create():
-    print("\n\n post \n route", flush=True)
-
+    request_data = request.json
+    try:
+        # Validate request body against schema data types
+        register_data = register_schema.load(request_data)
+    except ValidationError as err:
+        # Return a nice message if validation fails
+        return jsonify(err.messages), 400
     new_user_id = uuid.uuid4()
-    u_creds = UserCredentials(id=new_user_id, **request.json["credentials"])
 
-    u_data = UserData(user_id=new_user_id, **request.json["user_data"])
+    u_creds = UserCredentials(id=new_user_id, **register_data["credentials"])
+    u_data = UserData(user_id=new_user_id, **register_data["user_data"])
+    try:
+        db.session.add(u_creds)
+        db.session.commit()
+    except IntegrityError as err:
+        return str(err), 409
 
-    db.session.add(u_creds)
     db.session.add(u_data)
-
     db.session.commit()
-    return "ok"
+
+    data = {"user_id": new_user_id}
+    return jsonify(data), 201
 
 
 @create_bp.route("", methods=["GET"])
