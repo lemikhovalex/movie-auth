7c7b69b52f79d42d0fed01ebe4db130a8cdfc880
Gee Hjwić <ghj096@gmail.com>
2022-04-24 20:21:49 +0300

    Remove test route

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -7,8 +7,3 @@ v1_bp = Blueprint("v1", __name__, url_prefix="/v1")
 
 v1_bp.register_blueprint(users_bp)
 v1_bp.register_blueprint(auth_bp)
-
-
-@v1_bp.route("/hello")
-def hello_world():
-    return "<p>Hello, World!</p>"
