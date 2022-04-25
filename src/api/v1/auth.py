aeb57d89aa95a88168069721146ea5fd13931f80
Aleksandr Lemikhov <lemikhovalex@yandex.ru>
2022-04-25 16:11:01 +0300

    - add 404 to wrong login - rm temporaly check, widely used func

--- src/api/v1/auth.py
+++ src/api/v1/auth.py
@@ -4,18 +4,14 @@ from flask import Blueprint, jsonify, request
 from marshmallow import ValidationError
 
 from api.v1.users import get_roles
+from api.v1.utils import check as check_utils
 from config.db import REFRESH_TOKEN_EXP
 from db.pg import db
 from db.redis import ref_tok
 from models.sessions import Session
 from models.user import UserCredentials
 from schemas.user import login_schema
-from services.blacklist import (
-    ACCESS_REVOKED,
-    LOG_OUT_ALL,
-    UPD_PAYLOAD,
-    AccessForBlackList,
-)
+from services.blacklist import ACCESS_REVOKED, LOG_OUT_ALL, AccessForBlackList
 from utils.crypto import check_password
 from utils.tokens import check_validity_and_payload, get_access_and_refresh_jwt
 
@@ -35,6 +31,8 @@ def login():
     creds_from_storage = UserCredentials.query.filter_by(
         login=login_data["login"]
     ).first()
+    if creds_from_storage is None:
+        return "invalid login", 404
 
     if check_password(
         password=login_data["password"], password_encoded=creds_from_storage.password
@@ -45,7 +43,8 @@ def login():
         db.session.add(session_start)
         db.session.commit()
         access_token, refresh_token = get_access_and_refresh_jwt(
-            user_id=creds_from_storage.id, roles_getter=get_roles
+            user_id=creds_from_storage.id,
+            roles_getter=get_roles,
         )
         # add this refresh token to redis
         ref_tok.setex(
@@ -69,28 +68,7 @@ def login():
 
 @bp.route("/check", methods=["POST"])
 def check() -> (dict, list, int):
-    access_token = request.headers.get("Authorization") or ""
-    agent = request.headers.get("User-Agent")
-    new_token = None
-
-    is_valid, payload = check_validity_and_payload(access_token)
-    black_list_info = AccessForBlackList.from_dict(**payload)
-    if not is_valid:
-        return "go log in", 403
-    user_id = payload["user_id"]
-    if not ACCESS_REVOKED.is_ok(access_token):
-        print("revoked")
-        return ("revoked", 403)
-
-    if not LOG_OUT_ALL.is_ok(payload=black_list_info):
-        ACCESS_REVOKED.add(token=access_token)
-        return ("requested logout", 403)
-
-    if not UPD_PAYLOAD.is_ok(payload=black_list_info):
-        ACCESS_REVOKED.add(access_token)
-        new_token, _ = get_access_and_refresh_jwt(user_id, get_roles)
-        UPD_PAYLOAD.process_update(user_id=user_id, agent=agent)
-    return jsonify({"new_access_token": new_token, "roles": payload["roles"]}), 200
+    return check_utils(request=request, roles_getter=get_roles)
 
 
 @bp.route("/logout", methods=["POST"])
